16fb6872c11bea053b1c8796de3397adf861ccab
DonovanAdrian <websurfer21020@gmail.com>
2020-09-21 13:28:47 -0600

    Updated All Files
    Added authoring information on all files.

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/*
+ * This project was created by Donovan Adrian and any
+ * edits or changes must be confirmed as valid by Donovan
+ * with written consent under any circumstance.
+ */
+
+//Start Code Here
